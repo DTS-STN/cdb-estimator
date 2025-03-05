cf0296d4523fc2bc5c65de695ebac0b356ce8d87
Ken Blanchard <ken.blanchard@hrsdc-rhdcc.gc.ca>
2025-03-05 16:07:48 -0400

    Add estimator locale files

--- frontend/app/.server/locales/index.ts
+++ frontend/app/.server/locales/index.ts
@@ -1,3 +1,5 @@
+import estimatorEn from '~/.server/locales/estimator-en.json';
+import estimatorFr from '~/.server/locales/estimator-fr.json';
 import gcwebEn from '~/.server/locales/gcweb-en.json';
 import gcwebFr from '~/.server/locales/gcweb-fr.json';
 import publicEn from '~/.server/locales/public-en.json';
@@ -6,11 +8,13 @@ import publicFr from '~/.server/locales/public-fr.json';
 export const i18nResourcesEn = {
   gcweb: gcwebEn,
   public: publicEn,
+  estimator: estimatorEn,
 } as const;
 
 export const i18nResourcesFr = {
   gcweb: gcwebFr,
   public: publicFr,
+  estimator: estimatorFr,
 } as const;
 
 export const i18nResources = {
