dae8d03277f0cfe06fc7bd9e16378de21c51d6d9
Ken Blanchard <ken.blanchard@hrsdc-rhdcc.gc.ca>
2025-03-14 13:44:00 -0300

    refactor(frontend): Improve on age-utils and add tests

--- frontend/app/utils/age-utils.ts
+++ frontend/app/utils/age-utils.ts
@@ -1,19 +1,37 @@
-import { differenceInYears } from 'date-fns';
+import { differenceInMonths, differenceInYears } from 'date-fns';
 
 /**
- * Calculates a person's age on the current date.
- * @param birthMonth The person's birth month.
- * @param birthYear The person's birth year.
+ * Calculates a person's age in years on the current date.
+ * @param month The person's birth month as number between 1 and 12.
+ * @param year The person's birth year.
  
  * @returns The person's age (years) on the current day.
  */
-export function calculateAge(birthMonth: number, birthYear: number): number {
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#interpretation_of_two-digit_years
-  const birthDate = new Date();
-  birthDate.setFullYear(birthYear);
-  birthDate.setMonth(birthMonth - 1);
-  birthDate.setDate(1);
+export function calculateAge(month: number, year: number): number {
+  const date = new Date(year, month - 1, 1); // Explicity set to first day of the month
+  date.setUTCHours(0, 0, 0, 0); // Set the time to midnight because we only care about the date
+  date.setFullYear(year); // Ensure the year is set correctly for years < 100
 
-  const age = differenceInYears(new Date(), birthDate);
-  return age;
+  const now = new Date();
+  now.setUTCHours(0, 0, 0, 0); // Set the time to midnight because we only care about the date
+
+  return differenceInYears(now, date);
+}
+
+/**
+ * Calculates a person's age in months on the current date.
+ * @param month The person's birth month as number between 1 and 12.
+ * @param year The person's birth year.
+ 
+ * @returns The person's age (months) on the current day.
+ */
+export function calculateAgeInMonths(month: number, year: number): number {
+  const date = new Date(year, month - 1, 1); // Explicity set to first day of the month
+  date.setUTCHours(0, 0, 0, 0); // Set the time to midnight because we only care about the date
+  date.setFullYear(year); // Ensure the year is set correctly for years < 100
+
+  const now = new Date();
+  now.setUTCHours(0, 0, 0, 0); // Set the time to midnight because we only care about the date
+
+  return differenceInMonths(now, date);
 }
