f3b6c9afca3f6901066f92488d7f09dbad7607d3
Lewis <jonathan.lewis@hrsdc-rhdcc.gc.ca>
2025-03-31 15:27:41 -0300

    added footer component

---
+++ frontend/app/components/footer.tsx
@@ -0,0 +1,240 @@
+export function Footer() {
+  return (
+    <footer id="footer" className="bg-[#26374A]">
+      <section>
+        <h2 className="sr-only" id="accessibleSectionHeader1">
+          About Government
+        </h2>
+        <h3 className="container pt-[24px] pb-2 text-[19px] leading-[21px] font-bold text-white">Government of Canada</h3>
+        <div className="bg-[url('/footer_bg_img.svg')] bg-clip-border bg-bottom bg-no-repeat sm:bg-right-bottom">
+          <nav role="navigation" aria-labelledby="accessibleSectionHeader1">
+            <ul className="container flex flex-col pb-[22px] sm:grid sm:grid-cols-3">
+              <li className="footerLine relative my-1 mb-3 w-64 list-none pb-[26px] sm:w-56 lg:w-80">
+                <a className="text-[14px] leading-[19px] font-[400] !text-white" href="https://www.canada.ca/en/contact.html">
+                  All contacts
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/government/dept.html"
+                >
+                  Departments and agencies
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/government/system.html"
+                >
+                  About government
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/jobs.html"
+                >
+                  Jobs
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/taxes.html"
+                >
+                  Taxes
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://international.gc.ca/world-monde/index.aspx?lang=eng"
+                >
+                  Canada and the world
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/immigration-citizenship.html"
+                >
+                  Immigration and citizenship
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/environment.html"
+                >
+                  Environment and natural resources
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/finance.html"
+                >
+                  Money and finance
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a className="text-[14px] leading-[19px] font-[400] !text-white" href="https://travel.gc.ca/">
+                  Travel and tourism
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/defence.html"
+                >
+                  National security and defence
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/science.html"
+                >
+                  Science and innovation
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/business.html"
+                >
+                  Business
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/culture.html"
+                >
+                  Culture, history and sport
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/indigenous-peoples.html"
+                >
+                  Indigenous peoples
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/benefits.html"
+                >
+                  Benefits
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/policing.html"
+                >
+                  Policing, justice and emergencies
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/veterans-military.html"
+                >
+                  Veterans and military
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/health.html"
+                >
+                  Health
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/transport.html"
+                >
+                  Transport and infrastructure
+                </a>
+              </li>
+              <li className="my-1 w-64 list-none sm:w-56 lg:w-80">
+                <a
+                  className="text-[14px] leading-[19px] font-[400] !text-white"
+                  href="https://www.canada.ca/en/services/youth.html"
+                >
+                  Youth
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </section>
+      <section className="bg-[#F8F8F8]">
+        <div className="container flex min-h-[96px] justify-between">
+          <div className="flex flex-row items-center">
+            <nav role="navigation" aria-labelledby="accessibleSectionHeader2">
+              <h2 className="sr-only" id="accessibleSectionHeader2">
+                About this site
+              </h2>
+              <ul className="flex flex-col py-4 whitespace-nowrap md:flex-row">
+                <li className="my-1 pr-4">
+                  <a
+                    className="text-[14px] leading-[19px] font-[400] text-[#284162]"
+                    href="https://www.canada.ca/en/social.html"
+                  >
+                    Social media
+                  </a>
+                </li>
+                <li className="md:custom-bullet my-1 pr-4">
+                  <a
+                    className="text-[14px] leading-[19px] font-[400] text-[#284162]"
+                    href="https://www.canada.ca/en/mobile.html"
+                  >
+                    Mobile applications
+                  </a>
+                </li>
+                <li className="md:custom-bullet my-1 pr-4">
+                  <a
+                    className="text-[14px] leading-[19px] font-[400] text-[#284162]"
+                    href="https://www.canada.ca/en/government/about.html"
+                  >
+                    About Canada.ca
+                  </a>
+                </li>
+                <li className="md:custom-bullet my-1 pr-4">
+                  <a
+                    className="text-[14px] leading-[19px] font-[400] text-[#284162]"
+                    href="https://www.canada.ca/en/transparency/terms.html"
+                  >
+                    Terms and conditions
+                  </a>
+                </li>
+                <li className="md:custom-bullet my-1 pr-4">
+                  <a
+                    className="text-[14px] leading-[19px] font-[400] text-[#284162]"
+                    href="https://www.canada.ca/en/transparency/privacy.html"
+                  >
+                    Privacy
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="mr-[5px] flex min-h-[96px] shrink-0 items-end md:items-center">
+            <img
+              className="my-[15px] h-[25px] w-[105px] md:h-[40px] md:w-[164px]"
+              src="https://www.canada.ca/etc/designs/canada/wet-boew/assets/wmms-blk.svg"
+              alt="Symbol of the Government of Canada"
+            />
+          </div>
+        </div>
+      </section>
+    </footer>
+  );
+}
