39c211e788bbb4e0b0061479ce0655a484ff0be2
Ken Blanchard <118181922+ken-blanchard@users.noreply.github.com>
2025-04-15 12:50:15 -0300

    Update default Adobe Analytics script to use Authenticated (#196)

--- frontend/app/.server/environment/adobe-analytics.ts
+++ frontend/app/.server/environment/adobe-analytics.ts
@@ -5,7 +5,7 @@ import { stringToBooleanSchema } from '../validation/string-to-boolean-schema';
 export type Buildinfo = Readonly<v.InferOutput<typeof adobeAnalytics>>;
 
 export const defaults = {
-  ADOBE_ANALYTICS_SRC: 'https://assets.adobedtm.com/be5dfd287373/0127575cd23a/launch-913b1beddf7a-staging.min.js',
+  ADOBE_ANALYTICS_SRC: 'https://assets.adobedtm.com/be5dfd287373/9b9cb7867b5b/launch-cad75bf2f0d2-staging.min.js',
   ADOBE_ANALYTICS_JQUERY_SRC: 'https://code.jquery.com/jquery-3.7.1.min.js',
   ADOBE_ANALYTICS_ENABLED: 'true',
   ADOBE_ANALYTICS_DEBUG: 'false',
