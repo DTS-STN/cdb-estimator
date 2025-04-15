d9bd43f844d3d6e800a864c14796709d07580acc
Lewis <jonathan.lewis@hrsdc-rhdcc.gc.ca>
2025-04-15 14:48:01 -0300

    add e2e for single happy path

---
+++ frontend/e2e/single-results.ts
@@ -0,0 +1,24 @@
+import { expect, test } from '@playwright/test';
+
+test('Single person can obtain results', async ({ page }) => {
+    // splash page
+    await page.goto('/');
+  
+    // dashboard
+    await page.getByText(/english/i).click();
+    await page.getByText(/start/i).click();
+    
+    // marital-status
+    await page.getByRole('radio', { name: /single/i }).check();
+    await page.getByRole('button', { name: /next/i }).click();
+
+    // income
+    await page.getByRole('textbox', { name: /net income/i }).fill('23000.31');
+    await page.getByRole('textbox', { name: /working income/i }).fill('0');
+    await page.getByRole('textbox', { name: /^(?=.*uccb)(?=.*rdsp)(?=.*income).*/i }).fill('0');
+    await page.getByRole('textbox', { name: /^(?=.*uccb)(?=.*rdsp)(?=.*repayment).*/i }).fill('0');
+    await page.getByRole('button', { name: /estimate/i}).click();
+
+    // result
+    await expect(page.getByRole("main")).toContainText('$199.99');
+  });
