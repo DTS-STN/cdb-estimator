653750da26cf53f8a7631ac66f93978a4d96b2fc
Remy Mazerolle <remy.mazerolle@hrsdc-rhdcc.gc.ca>
2025-04-17 15:51:49 -0300

    adds a11y checks for all pages, adds supports for staging session data

--- frontend/e2e/not-found.spec.ts
+++ frontend/e2e/not-found.spec.ts
@@ -1,3 +1,4 @@
+import AxeBuilder from '@axe-core/playwright';
 import { expect, test } from '@playwright/test';
 
 test('Navigating to /foo renders the bilingual 404 page', async ({ page }) => {
@@ -14,4 +15,22 @@ test('Navigating to /en/foo renders the unilingual 404 page', async ({ page }) =
   expect(await page.locator('header').innerHTML()).toMatchSnapshot();
   expect(await page.locator('main').innerHTML()).toMatchSnapshot();
   expect(await page.locator('footer').innerHTML()).toMatchSnapshot();
+});
+
+test('Bilingual 404 page passes a11y checks', async ({ page }) => {
+  await page.goto('/foo');
+  await page.locator('main').waitFor();
+
+  const accessibilityScanResults = await new AxeBuilder({ page }).include('main').analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
+
+test('Unilingual 404 page passes a11y checks', async ({ page }) => {
+  await page.goto('/en/foo');
+  await page.locator('main').waitFor();
+
+  const accessibilityScanResults = await new AxeBuilder({ page }).include('main').analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
 });
