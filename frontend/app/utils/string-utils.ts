12a64748bae56e2195e5e79a36fe006a2f2541f6
Remy Mazerolle <remy.mazerolle@hrsdc-rhdcc.gc.ca>
2025-04-24 14:35:35 -0300

    code cleanup: remove commented out function

--- frontend/app/utils/string-utils.ts
+++ frontend/app/utils/string-utils.ts
@@ -1,22 +1,8 @@
 /**
  * Strips formatting from a string representing a formatted decimal
  * @param input string represenation of a formatted decimal (examples: en:"1,234.56", fr:"1 234,56")
- * @param lang Format language (en or fr)
  * @returns string representation of the decimal without formatting (examples: "1234.56")
- *
-export function removeNumericFormatting(input: string, lang: Language): string {
-  switch (lang) {
-    case 'en': {
-      const output = input.replaceAll(',', '');
-      return output;
-    }
-    case 'fr': {
-      const output = input.replaceAll(' ', '').replaceAll(',', '.');
-      return output;
-    }
-  }
-}
-*/
+ */
 export function removeNumericFormatting(input: string | undefined): string {
   if (input === undefined) return '';
 
