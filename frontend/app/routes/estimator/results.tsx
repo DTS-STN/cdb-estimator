7687d561affd3b43be54d2c23319b031ca5b5508
jonathan-lewis-esdc <jonathan.lewis@hrsdc-rhdcc.gc.ca>
2025-04-25 09:02:06 -0300

    show single-divorced estimate text when the married estimate and split benefit is the same ammount (#251)

--- frontend/app/routes/estimator/results.tsx
+++ frontend/app/routes/estimator/results.tsx
@@ -127,6 +127,11 @@ export default function Results({ actionData, loaderData, matches, params }: Rou
     ESTIMATOR_CDB_URL_EN,
     ESTIMATOR_CDB_URL_FR,
   } = globalThis.__appEnvironment;
+  const estimationEqualsSplitBenefits =
+    loaderData.formattedResults.results.kind === 'married' &&
+    loaderData.formattedResults.results.estimationSplitBenefit === loaderData.formattedResults.results.estimation;
+
+  console.log(estimationEqualsSplitBenefits);
 
   const cdbContactLink = (
     <InlineLink
@@ -149,7 +154,7 @@ export default function Results({ actionData, loaderData, matches, params }: Rou
           <section className="col-span-2 row-span-1 space-y-6">
             <h2 className="font-lato mb-4 text-lg font-bold">{t('estimator:results.content.your-estimate.header')}</h2>
 
-            {loaderData.results.maritalStatus === 'single-divorced-separated-or-widowed' && (
+            {(loaderData.results.maritalStatus === 'single-divorced-separated-or-widowed' || estimationEqualsSplitBenefits) && (
               <>
                 <p className="mb-4">{t('estimator:results.content.your-estimate.single.intro')}</p>
                 <ul className="list-disc space-y-1 pl-7">
@@ -164,7 +169,7 @@ export default function Results({ actionData, loaderData, matches, params }: Rou
               </>
             )}
 
-            {loaderData.formattedResults.results.kind === 'married' && (
+            {loaderData.formattedResults.results.kind === 'married' && !estimationEqualsSplitBenefits && (
               <>
                 <p className="mb-4">{t('estimator:results.content.your-estimate.married-common-law.intro')}</p>
                 <ul className="list-disc space-y-1 pl-7">
@@ -175,6 +180,7 @@ export default function Results({ actionData, loaderData, matches, params }: Rou
                       values={{ result: loaderData.formattedResults.results.estimation }}
                     />
                   </li>
+
                   <li>
                     <Trans
                       ns={handle.i18nNamespace}
