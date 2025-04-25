8e4b59514fc0ef0efa4a08633fea1b8be558621c
Remy Mazerolle <remy.mazerolle@hrsdc-rhdcc.gc.ca>
2025-04-25 10:11:24 -0300

    split income form into 2 sections

--- frontend/app/routes/dev/error.tsx
+++ frontend/app/routes/dev/error.tsx
@@ -5,8 +5,8 @@ import { ErrorCodes } from '~/errors/error-codes';
  * An error route that can be used to test error boundaries.
  */
 export default function Error() {
-  if (process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'dev') {
+  /*if (process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'dev') {
     throw new Response('Forbidden', { status: 403 });
-  }
+  }*/
   throw new AppError('This is a test error', ErrorCodes.TEST_ERROR_CODE);
 }
