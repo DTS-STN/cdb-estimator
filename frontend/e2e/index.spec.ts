0a5e17369a10335b1ec98fb694a897ec87358f03
Ken Blanchard <118181922+ken-blanchard@users.noreply.github.com>
2025-04-29 14:19:05 -0300

    e2e(frontend) Improve some E2E tests (#272)

--- frontend/e2e/index.spec.ts
+++ frontend/e2e/index.spec.ts
@@ -2,33 +2,44 @@ import AxeBuilder from '@axe-core/playwright';
 import { expect, test } from '@playwright/test';
 
 import { formatHtml } from './__supports/string-utils';
+import { PlaywrightEstimatorPage } from './models/PlaywrightEstimatorPage';
 
 test('Navigating to /en renders the english dashboard page', async ({ page }) => {
+  const estimator = new PlaywrightEstimatorPage(page);
+
   await page.goto('/en');
+  await estimator.isLoaded('index');
 
   expect(await formatHtml(await page.locator('main').innerHTML())).toMatchSnapshot();
 });
 
 test('Navigating to /fr renders the french dashboard page', async ({ page }) => {
+  const estimator = new PlaywrightEstimatorPage(page);
+
   await page.goto('/fr');
+  await estimator.isLoaded('index', 'fr');
 
   expect(await formatHtml(await page.locator('main').innerHTML())).toMatchSnapshot();
 });
 
 test('/en passes a11y checks', async ({ page }) => {
-  await page.goto('/en');
-  await page.locator('main').waitFor();
+  const estimator = new PlaywrightEstimatorPage(page);
 
-  const accessibilityScanResults = await new AxeBuilder({ page }).include('main').analyze();
+  await page.goto('/en');
+  await estimator.isLoaded('index');
+
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
   expect(accessibilityScanResults.violations).toEqual([]);
 });
 
 test('/fr passes a11y checks', async ({ page }) => {
-  await page.goto('/fr');
-  await page.locator('main').waitFor();
+  const estimator = new PlaywrightEstimatorPage(page);
 
-  const accessibilityScanResults = await new AxeBuilder({ page }).include('main').analyze();
+  await page.goto('/fr');
+  await estimator.isLoaded('index', 'fr');
+
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
   expect(accessibilityScanResults.violations).toEqual([]);
 });
