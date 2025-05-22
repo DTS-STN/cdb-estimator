c83204bec218392370ab838dd3618779d7cc649e
jonathan-lewis-esdc <jonathan.lewis@hrsdc-rhdcc.gc.ca>
2025-05-22 13:33:21 -0300

    fix currency field currency symbol accessibility (#338)

--- frontend/app/components/currency-field.tsx
+++ frontend/app/components/currency-field.tsx
@@ -13,7 +13,7 @@ export interface CurrencyFieldProps extends Omit<InputFieldProps, 'type' | 'valu
   defaultValue?: string | number | null;
 }
 
-export function CurrencyField({ allowNegative = false, maxLength, defaultValue, ...rest }: CurrencyFieldProps) {
+export function CurrencyField({ allowNegative = false, maxLength, defaultValue, label, ...rest }: CurrencyFieldProps) {
   const { i18n } = useTranslation(['common']);
   const strippedDefaultValue = Number(removeNumericFormatting(defaultValue?.toString()));
   const val = Number.isInteger(strippedDefaultValue) ? strippedDefaultValue : strippedDefaultValue.toFixed(2);
@@ -30,10 +30,28 @@ export function CurrencyField({ allowNegative = false, maxLength, defaultValue, 
       thousandsGroupStyle={'thousand'}
       maxLength={maxLength ?? (i18n.language === 'en' ? 15 : 16)}
       type="text"
-      beforeInput={i18n.language === 'en' ? <span className="mr-1">$</span> : undefined}
-      afterInput={i18n.language === 'fr' ? <span className="ml-1">$</span> : undefined}
+      beforeInput={
+        i18n.language === 'en' ? (
+          <span aria-hidden="true" className="mr-1">
+            $
+          </span>
+        ) : undefined
+      }
+      afterInput={
+        i18n.language === 'fr' ? (
+          <span aria-hidden="true" className="ml-1">
+            $
+          </span>
+        ) : undefined
+      }
       className={cn('inline', rest.className)}
       defaultValue={val}
+      label={
+        <>
+          {label}
+          <span className="sr-only"> ($)</span>
+        </>
+      }
       {...rest}
     />
   );
