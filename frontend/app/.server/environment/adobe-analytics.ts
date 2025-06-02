bd3f8e87787855208ce12f81eb26b2cf551eed62
remy-mazerolle-esdc <remy.mazerolle@hrsdc-rhdcc.gc.ca>
2025-06-02 12:40:56 -0300

    update AA config, service name, error name, script version (#359)

--- frontend/app/.server/environment/adobe-analytics.ts
+++ frontend/app/.server/environment/adobe-analytics.ts
@@ -5,12 +5,12 @@ import { stringToBooleanSchema } from '../validation/string-to-boolean-schema';
 export type Buildinfo = Readonly<v.InferOutput<typeof adobeAnalytics>>;
 
 export const defaults = {
-  ADOBE_ANALYTICS_SRC: 'https://assets.adobedtm.com/be5dfd287373/9b9cb7867b5b/launch-cad75bf2f0d2-staging.min.js',
+  ADOBE_ANALYTICS_SRC: 'https://assets.adobedtm.com/be5dfd287373/0127575cd23a/launch-913b1beddf7a-staging.min.js',
   ADOBE_ANALYTICS_JQUERY_SRC: 'https://code.jquery.com/jquery-3.7.1.min.js',
   ADOBE_ANALYTICS_ENABLED: 'true',
   ADOBE_ANALYTICS_DEBUG: 'false',
-  ADOBE_ANALYTICS_SERVICE_NAME: 'ESDC-EDSC_CDB-PCH',
-  ADOBE_ANALYTICS_ERROR_NAME: 'ESDC-EDSC:CDB-estimator',
+  ADOBE_ANALYTICS_SERVICE_NAME: 'ESDC-EDSC_CDB-PCH-EST',
+  ADOBE_ANALYTICS_ERROR_NAME: 'ESDC-EDSC_CDB-PCH-EST',
 } as const;
 
 export const adobeAnalytics = v.object({
